ef910953b9e93c23395bce0e04cd56a8d0a2ff8f
darchap <113114289+darchap@users.noreply.github.com>
2022-12-07 18:43:49 +0100

    Added option to check SSH port and some minor fixes

--- 0-runasroot.sh
+++ 0-runasroot.sh
@@ -35,30 +35,43 @@ function check_root() {
 }
 
 function check_ssh_root() {
+    echo -e 'Checking if you have root access trhough SSH...'
     if grep -q '^PermitRootLogin.yes' /etc/ssh/sshd_config; then
         read -rp 'You have enabled root login through SSH, do you wish to disable it now?[y/n]: ' yn
         case $yn in
         [Yy]*) sudo sed -i '/PermitRootLogin/c\#PermitRootLogin no' /etc/ssh/sshd_config ;;
-        [Nn]*) echo -e "\e[5mIs strongly advised to disable root login!\e[25m\n" ;;
+        [Nn]*) echo -e "\e[5mIs strongly advised to disable root login! \e[25m\n" ;;
         *) echo "Please answer yes or no." ;;
         esac
+    else
+        echo 'All OK!'
+    fi
+}
 
+function check_ssh_port() {
+    echo -e 'Checking your SSH port...'
+    if grep -q '#Port 22' /etc/ssh/sshd_config; then
+        read -rp 'It seems that your SSH port is the default one, do you wish to change it now?[y/n]: ' yn
+        case $yn in
+        [Yy]*)
+            read -rp 'Enter your desired SSH port: ' port
+            sudo sed -i "/#Port 22/c\Port $port" /etc/ssh/sshd_config
+            ;;
+        [Nn]*) echo -e "\e[5mIs strongly advised to change your SSH default port! \e[25m\n" ;;
+        *) echo "Please answer yes or no." ;;
+        esac
+    else
+        echo 'All OK!'
     fi
 }
 
 function check_distro() {
-    case $(. /etc/os-release && echo "$ID") in
-    *debian*)
-        distro="debian"
-        ;;
-    *ubuntu*)
-        distro="ubuntu"
-        ;;
-    *raspbian*)
-        distro="raspbian"
-        ;;
+    case $(lsb_release -i | awk '{print $3}') in
+    *[Dd]ebian*) ;;
+    *[Uu]buntu*) ;;
+    *[Rr]aspbian*) ;;
     *)
-        error "Not supported distro"
+        error "Not supported distro."
         exit 1
         ;;
     esac
@@ -132,17 +145,20 @@ add_user
 # 2 Install docker, watchtower and portainer as user and then reboot
 if [[ $useradded -eq 1 ]]; then
     if [[ $(grep "^$username.*ALL=(ALL).*NOPASSWD:.*ALL" /etc/sudoers) ]]; then
-        sudo -u $username bash $userscript
+        sudo -u "$username" bash $userscript
     elif [[ $(grep "^$username.*ALL=(ALL)" /etc/sudoers) ]]; then
         sed -i "/^$username ALL=(ALL)/d" /etc/sudoers
         echo "$username ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers >/dev/null
-        sudo -u $username bash $userscript
+        sudo -u "$username" bash $userscript
         sed -i "s/^$username ALL=(ALL).*/$username ALL=(ALL)/" /etc/sudoers
     else
         echo "$username ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers >/dev/null
-        sudo -u $username bash $userscript
+        sudo -u "$username" bash $userscript
     fi
     if [[ $? -eq 0 ]]; then
+        echo '\e[4mSSH (changes will take effect on next boot).\e[24m'
+        check_ssh_root
+        check_ssh_port
         timeout_reboot
     else
         echo "Cant finish the install."
